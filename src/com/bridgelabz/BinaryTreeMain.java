3ccdc5f91ecc131d827db30c2fad7d2fb0adf817
drisyaathul <drisyak809@gmail.com>
2023-05-02 01:25:35 +0800

    Added: To check all added Using size method

--- src/com/bridgelabz/BinaryTreeMain.java
+++ src/com/bridgelabz/BinaryTreeMain.java
@@ -6,6 +6,14 @@ public class BinaryTreeMain {
         binaryTree.add(56);
         binaryTree.add(30);
         binaryTree.add(70);
+
+        binaryTree.add(25);
+        binaryTree.add(80);
+        binaryTree.add(15);
+        binaryTree.add(60);
+
+        binaryTree.add(75);
+        binaryTree.add(20);
         binaryTree.display(binaryTree.root);
     }
 }
